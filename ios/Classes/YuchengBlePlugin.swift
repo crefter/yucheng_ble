4e2798b9dc84683021bb431c357b50deb237bdbf
Zarechnev Maxim <crefter1@yandex.ru>
2025-10-07 16:52:29 +0700

    Minor fix

--- ios/Classes/YuchengBlePlugin.swift
+++ ios/Classes/YuchengBlePlugin.swift
@@ -35,19 +35,18 @@ public class YuchengBlePlugin: NSObject, FlutterPlugin {
         if (updateStreamHandler == nil) {
             updateStreamHandler = UpdateDataHandlerImpl()
         }
-        
-        
-        YCProduct.realTimeDataUplod(YCProduct.shared.currentPeripheral,
-                                    isEnable: true,
-                                    dataType: YCRealTimeDataType.combinedData,
-                                    completion: {state, result in
-            if state == .succeed {
-                
-            } else {
-                
-            }
-        } );
-        
+
+//         YCProduct.realTimeDataUplod(YCProduct.shared.currentPeripheral,
+//                                             isEnable: true,
+//                                             dataType: YCRealTimeDataType.combinedData,
+//                                             completion: {state, result in
+//                     if state == .succeed {
+//
+//                     } else {
+//
+//                     }
+//                 } );
+
         DevicesStreamHandler.register(with: registrar.messenger(), streamHandler: devicesHandler!)
         SleepDataStreamHandler.register(with: registrar.messenger(), streamHandler: sleepDataHandler!)
         DeviceStateStreamHandler.register(with: registrar.messenger(), streamHandler: deviceStateStreamHandler!)
