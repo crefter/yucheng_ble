3349e9f82bd53e1f1b687ae74cbdd09c1b256b00
Zarechnev Maxim <crefter1@yandex.ru>
2026-03-30 17:34:38 +0700

    [Android] Set default tokenType = bearer

--- android/src/main/kotlin/com/crefter/yuchengplugin/yucheng_ble/entity/YuchengAuthToken.kt
+++ android/src/main/kotlin/com/crefter/yuchengplugin/yucheng_ble/entity/YuchengAuthToken.kt
@@ -9,7 +9,7 @@ import kotlin.time.Instant
 @Serializable
 data class YuchengAuthToken(
     val accessToken: String,
-    val tokenType: String? = null,
+    val tokenType: String? = "bearer",
     val refreshToken: String? = null,
     val issuedAt: Instant? = null,
     val expiresAt: Instant? = null,
