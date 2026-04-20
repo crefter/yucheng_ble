67d76d7fd82f050971ea5b4d5a7f9554dc6772fc
Zarechnev Maxim <crefter1@yandex.ru>
2026-04-20 17:49:52 +0700

    Bump Set Bearer

--- android/src/main/kotlin/com/crefter/yuchengplugin/yucheng_ble/entity/YuchengAuthToken.kt
+++ android/src/main/kotlin/com/crefter/yuchengplugin/yucheng_ble/entity/YuchengAuthToken.kt
@@ -9,7 +9,7 @@ import kotlin.time.Instant
 @Serializable
 data class YuchengAuthToken(
     val accessToken: String,
-    val tokenType: String? = "bearer",
+    val tokenType: String = "Bearer",
     val refreshToken: String? = null,
     val issuedAt: Instant? = null,
     val expiresAt: Instant? = null,
